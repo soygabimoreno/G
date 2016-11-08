a779396d04f4cc9f95ad14962ec43feec6f5b766
Gabriel_Moreno <android@planetdevices.com>
2016-11-08 20:48:17 +0100

    Dealing with problems uploading

---
+++ g/src/main/java/com/gabrielmorenoibarra/g/G.java
@@ -0,0 +1,50 @@
+package com.gabrielmorenoibarra.g;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
+import android.os.Build;
+import android.util.Log;
+
+/**
+ * Android Tools.
+ * Created by Gabriel Moreno on 2016-11-08.
+ */
+public class G {
+
+    /**
+     * Check for all possible Internet providers.
+     * @param context Related context.
+     * @return true if there is an available connection.
+     */
+    public static boolean isConnectedToInternet(Context context) {
+        final String TAG = Thread.currentThread().getStackTrace()[2].getMethodName();
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) { // If we are upper API 21
+            Network[] networks = connectivityManager.getAllNetworks();
+            NetworkInfo networkInfo;
+            for (Network mNetwork : networks) {
+                networkInfo = connectivityManager.getNetworkInfo(mNetwork);
+                if (networkInfo.getState().equals(NetworkInfo.State.CONNECTED)) {
+                    Log.d(TAG, "Network " + networkInfo.getTypeName() + " connected.");
+                    return true;
+                }
+            }
+        } else { // API < 21
+            if (connectivityManager != null) {
+                // noinspection deprecation
+                NetworkInfo[] info = connectivityManager.getAllNetworkInfo();
+                if (info != null) {
+                    for (NetworkInfo anInfo : info) {
+                        if (anInfo.getState() == NetworkInfo.State.CONNECTED) {
+                            Log.d(TAG, "Network " + anInfo.getTypeName() + " connected.");
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
