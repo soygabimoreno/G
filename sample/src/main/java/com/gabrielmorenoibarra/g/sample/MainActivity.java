939edf16494d7c4a27f431bcb6e0b142e60861c9
Gabriel_Moreno <android@planetdevices.com>
2016-11-24 22:47:36 +0100

    g is compiled as a module

--- sample/src/main/java/com/gabrielmorenoibarra/g/sample/MainActivity.java
+++ sample/src/main/java/com/gabrielmorenoibarra/g/sample/MainActivity.java
@@ -13,8 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TODO: I have to set g as a module for sample 
-
         if (G.isConnectedToInternet(this)) {
             Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
         }
